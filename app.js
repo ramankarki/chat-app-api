2fbc559f754c230c7fd0afd255321eb3cacc87ec
ramankarki <ramankarki40@gmail.com>
2021-02-13 21:07:51 +0545

    initialied globalErrorHandler middleware at the end

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const globalErrorHandler = require("./controller/globalErrorHandler");
+
 if (process.env.NOD_ENV !== "production") {
   const morgan = require("morgan");
   app.use(morgan("dev"));
@@ -11,5 +13,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("app is running");
 });
+
+app.all("*", (req, res, next) => {
+  next(new AppError(404, `Can't find ${req.originalUrl} on this API`));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
