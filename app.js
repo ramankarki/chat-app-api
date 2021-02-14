113129517cd553355cca7f679463bc184606b56a
ramankarki <ramankarki40@gmail.com>
2021-02-14 13:09:49 +0545

    imported userRoutes, AppError creator and initialized in middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controller/globalErrorHandler");
 
 if (process.env.NOD_ENV !== "production") {
@@ -10,9 +12,7 @@ if (process.env.NOD_ENV !== "production") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("app is running");
-});
+app.use("/api/v1/users", userRoutes);
 
 app.all("*", (req, res, next) => {
   next(new AppError(404, `Can't find ${req.originalUrl} on this API`));
